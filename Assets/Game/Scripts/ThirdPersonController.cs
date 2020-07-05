bc039971ff0e6c5d1d902025f8933026093ed719
Gabriel Cencic <gabriel.cencic@gmail.com>
2020-07-05 03:47:10 -0300

    Add roll feature to Character Controller

--- Assets/Game/Scripts/ThirdPersonController.cs
+++ Assets/Game/Scripts/ThirdPersonController.cs
@@ -1,26 +1,40 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Game
 {
     public class ThirdPersonController : MonoBehaviour
     {
+        public Transform mainCamera;
+
+        [Header("Walk/Run Settings")]
         public float walkSpeed = 2.0f;
-
         public float runSpeed = 4.0f;
-
-        private float speed;
-
         public float turnSmooth = 0.1f;
         public float runSmooth = 0.3f;
-
-        public Transform cam; // Main camera, not vcam
-
         private float turnSmoothVelocity;
+        private float speed;
 
+        [Header("Roll Settings")]
+        public float rollCooldown = 0.9f;
+
+        [Header("Info")]
         private CharacterController controller;
-
         private Animator animator;
+
+        [SerializeField]
+        [ReadOnly]
+        /* As long as cooldown is not equal to 0, player movements are disabled.*/
+        private int cooldownCount = 0;
+
+        public bool HasCooldown
+        {
+            get
+            {
+                return cooldownCount > 0;
+            }
+        }
 
         void Start()
         {
@@ -35,27 +49,33 @@ namespace Game
 
         void Update()
         {
+            if (HasCooldown) return;
+
             float h = Input.GetAxis("Horizontal");
             float v = Input.GetAxis("Vertical");
 
-
-            if (Input.GetButton("Fire1") || Input.GetButton("Fire3"))
+            Vector3 direction = new Vector3(h * speed, 0f, v * speed);
+            Vector3 clampedDir = Vector3.ClampMagnitude(direction, speed); // We don't want to move faster in diagonal directions
+            if (Input.GetButtonDown("Fire2") || Input.GetButtonDown("Jump")) // Roll
+            {
+                controller.radius = 0.5f;
+                animator.SetTrigger("Rolling");
+                AddCooldown(rollCooldown);
+            }
+            else if (Input.GetButton("Fire1") || Input.GetButton("Fire3")) // Run
             {
                 speed = Mathf.SmoothStep(speed, runSpeed, runSmooth);
                 controller.radius = 0.55f;
             }
-            else
+            else // Simple walk
             {
                 speed = Mathf.SmoothStep(speed, walkSpeed, runSmooth);
                 controller.radius = 0.4f;
             }
 
-            Vector3 direction = new Vector3(h * speed, 0f, v * speed);
-            Vector3 clampedDir = Vector3.ClampMagnitude(direction, speed); // We don't want to move faster in diagonal directions
-
             if (clampedDir.magnitude >= 0.01) // Minimum movement threshold
             {
-                float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+                float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + mainCamera.eulerAngles.y;
                 float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmooth);
                 transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
@@ -63,6 +83,19 @@ namespace Game
                 controller.Move(moveDir.normalized * clampedDir.magnitude * Time.deltaTime);
             }
             animator.SetFloat("Speed", clampedDir.magnitude / runSpeed);
+        }
+        public void AddCooldown(float duration)
+        {
+            StartCoroutine(_AddCooldown(duration));
+        }
+
+        private IEnumerator _AddCooldown(float duration)
+        {
+            cooldownCount++;
+
+            yield return new WaitForSeconds(duration);
+
+            cooldownCount--;
         }
     }
 }

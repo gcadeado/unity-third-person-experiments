b2d738ed87a4f7f2144de1688edc0f0783ca3045
Gabriel Cencic <gabriel.cencic@gmail.com>
2020-07-12 17:24:38 -0300

    Add custpom spawner and changes to cinemachine behavior

---
+++ Assets/Game/Scripts/CubesSpawner.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class CubesSpawner : MonoBehaviour
+{
+    public GameObject cube;
+    void Start()
+    {
+        for (int i = 0; i < 300; i++)
+        {
+            GameObject newCube = Instantiate(cube, transform.position + new Vector3(Random.Range(-200f, 200f), 0, Random.Range(10f, 250f)), Quaternion.identity, transform );
+            newCube.transform.localScale = new Vector3(3.0f, 3.0f, 3.0f);
+        }
+    }
+
+}
